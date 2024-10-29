749381426375f0a062475c4b30b57de3593917d2
petzl300lumen <Ftgaq007@gmail.com>
2024-10-29 21:27:34 +0300

    new commit

---
+++ my-app/src/YandexReviews.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+export default function YandexReviews() {
+  return (
+<>
+
+{/* <iframe src="https://swdgts.ru/cb3ebe3aed7008a3a9d44e02b3689d93" width="100%" height="700" frameborder="0"></iframe> */}
+
+{/* <div className='Review' style={{width: '460px' , height:' 650px', overflow: 'hidden', position: 'relative'}}>
+    <iframe style={{ width:'100%',height:'100%', border:'1px solid #e6e6e6', borderRadius:'20px', boxSizing:'border-box'}} src="https://yandex.ru/maps-reviews-widget/1004814134?comments">
+    </iframe>
+    <a href="https://yandex.ru/maps/org/avtoshkola_avangard/1004814134/" target="_blank" style={{textDecoration: 'none', color: '#b3b3b3' , fontSize: '10px', fontFamily: 'YS Text,sans-serif', padding:'0 20px', position:'absolute', bottom:'8px', width:'100%' , textAlign: 'center', left:'0', overflow: 'hidden', textOverflow:'ellipsis', display:'block', maxHeight:'14px', whiteSpace:'nowrap', padding:'0 16px',  boxSizing:'border-box'}}>Автошкола Авангард на карте Волгограда — Яндекс Карты
+    </a>
+</div>
+
+<div className='Review' style={{width: '460px', height: '650px', overflow: 'hidden', position: 'relative'}}>
+    <iframe style={{width: '100%', height: '100%', border: '1px solid #e6e6e6', borderRadius: '20px', boxSizing: 'border-box'}} src="https://yandex.ru/maps-reviews-widget/1070929987?comments">
+    </iframe>
+    <a href="https://yandex.ru/maps/org/avangard/1070929987/" target="_blank" style={{boxSizing: 'border-box', textDecoration: 'none', color: '#b3b3b3', fontSize: '10px', fontFamily: 'YS Text,sans-serif', padding: '0 16px', position: 'absolute', bottom: '8px', width: '100%', textAlign: 'center', left: 0, overflow: 'hidden', textOverflow: 'ellipsis', display: 'block', maxHeight: '14px', whiteSpace: 'nowrap'}}>Авангард на карте Волгограда — Яндекс Карты</a>
+</div>
+
+<div className='Review' style={{width: '460px', height: '650px', overflow: 'hidden', position: 'relative'}}>
+    <iframe style={{width: '100%', height: '100%', border: '1px solid #e6e6e6', borderRadius: '20px', boxSizing: 'border-box'}} src="https://yandex.ru/maps-reviews-widget/1098104028?comments">
+    </iframe>
+    <a href="https://yandex.ru/maps/org/avtoshkola_avangard/1098104028/" target="_blank" style={{boxSizing: 'border-box', textDecoration: 'none', color: '#b3b3b3', fontSize: '10px', fontFamily: 'YS Text,sans-serif', padding: '0 16px', position: 'absolute', bottom: '8px', width: '100%', textAlign: 'center', left: 0, overflow: 'hidden', textOverflow: 'ellipsis', display: 'block', maxHeight: '14px', whiteSpace: 'nowrap'}}>Автошкола Авангард на карте Волгограда — Яндекс Карты</a>
+    </div> */}
+
+</>
+  )
+}
+
+
+
+
